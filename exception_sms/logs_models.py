c37d2d22695487f5ef12bb0dbdcffa9eed80649c
thchoonlophon <qianchenjin192518@gmail.com>
2020-07-10 14:23:46 +0800

    Integrate log mechanism into SMS sending module

--- exception_sms/logs_models.py
+++ exception_sms/logs_models.py
@@ -6,10 +6,13 @@
 @file:   logs_models.py
 @time:   2020-07-09 13:15
 """
+from traceback import format_exc
+
+from exception_sms.to_get_logs import FinalLogger
 
 
 class GetNotification(object):
-    def __init__(self, access_key, secret_key, template_id, author, phone: str or list):
+    def __init__(self, access_key, secret_key, template_id, author, phone: str or list, log_path):
         from qiniu import QiniuMacAuth, Sms
         self.access_key = access_key
         self.secret_key = secret_key
@@ -18,10 +21,12 @@ class GetNotification(object):
         self.phone = self.get_phone_list(phone)
         self.auth = QiniuMacAuth(self.access_key, self.secret_key)
         self.smser = Sms(self.auth)
+        self.log_path = log_path
 
-    def get_phone_list(self, phone):
+    @staticmethod
+    def get_phone_list(phone):
         if type(phone) == str:
-            if "," in self.phone:
+            if "," in phone:
                 phone_list = phone.split(",")
             else:
                 phone_list = [phone]
@@ -35,17 +40,18 @@ class GetNotification(object):
             try:
                 func(*args, **kwargs)
             except Exception as e:
-                self.smser.sendMessage(self.template_id, self.phone, self.get_params(e,name))
+                self.smser.sendMessage(self.template_id, self.phone, self.get_params(e, name))
+                logger = FinalLogger(self.log_path)
+                logger.get_logs(format_exc())
             finally:
                 return res
+
         return get_info
 
-    def get_params(self,exece,func_name):
+    def get_params(self, exece, func_name):
         params = dict()
-        params["api_name"]=f"{func_name} 函数"
+        params["api_name"] = f"{func_name} 函数"
         params["name"] = self.author
         message = f"{func_name} 函数报错 {exece}"
         params["code"] = message
         return params
-
-
