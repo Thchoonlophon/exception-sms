85a5dfeacc4290e80d5c59a5cc9d53db0b9b4fd8
thchoonlophon <qianchenjin192518@gmail.com>
2020-07-13 16:41:16 +0800

    change the version

--- exception_sms/__init__.py
+++ exception_sms/__init__.py
@@ -10,7 +10,7 @@
 from exception_sms.logs_models import GetNotification
 from exception_sms.to_get_logs import FinalLogger
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     "GetNotification",
     "FinalLogger"
