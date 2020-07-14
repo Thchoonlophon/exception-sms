2414d5f5a629a642d50f44b9419c6a4e7dd76a05
thchoonlophon <qianchenjin192518@gmail.com>
2020-07-14 09:17:14 +0800

    change the file for email

--- exception_sms/send_email_module.py
+++ exception_sms/send_email_module.py
@@ -11,7 +11,6 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
-from traceback import format_exc
 
 lpath_os = os.path.dirname(os.path.abspath(__file__))
 
@@ -47,11 +46,11 @@ class SendMail(object):
         # 处理附件
         if self.file:
             file_path = '{}'.format(os.path.abspath(self.file))
-            file_name = file_path.replace(str(lpath_os) + '\\', '')
+            file_name = file_path.split("\\")[-1]
             att = MIMEApplication(open(self.file, 'rb').read())
             att.add_header('Content-Disposition', 'attachment', filename=file_name)
             msg.attach(att)
-            msg.attach(att)
+            # msg.attach(att)
         # 邮件正文的内容
         msg.attach(MIMEText(self.content, self.form1, self.form2))
         # 邮件主题
@@ -60,14 +59,16 @@ class SendMail(object):
         msg['From'] = Header('衡泰智信<%s>' % self.username, 'utf-8')
         # 接收者账号列表
         msg['To'] = Header('%s<%s>' % (self.recv_name, self.recv), 'utf-8')
-        if type(self.cc) == list:
-            st = [f'{self.cc_name[i]}<{self.cc[i]}>' for i in range(len(self.cc))]
-            string = ", ".join(st)
-            msg["Cc"] = Header(string, 'utf-8')
-            members = [self.recv] + self.cc
-        else:
-            msg["Cc"] = Header(f'{self.cc_name}<{self.cc}>', 'utf-8')
-            members = [self.recv, self.cc]
+        members = [self.recv]
+        if self.cc:
+            if type(self.cc) == list:
+                st = [f'{self.cc_name[i]}<{self.cc[i]}>' for i in range(len(self.cc))]
+                string = ", ".join(st)
+                msg["Cc"] = Header(string, 'utf-8')
+                members += self.cc
+            else:
+                msg["Cc"] = Header(f'{self.cc_name}<{self.cc}>', 'utf-8')
+                members.append(self.cc)
 
         # 发送邮件服务器的对象
         self.smtp.login(self.username, self.passwd)
@@ -75,23 +76,8 @@ class SendMail(object):
             print(members)
             self.smtp.sendmail(self.username, members, msg.as_string())
         except Exception as e:
-            print(format_exc())
+            print(e)
 
     def __del__(self):
         if self.smtp:
             self.smtp.quit()
-
-
-if __name__ == '__main__':
-    a = SendMail(
-        "jingxuan@lynchow.com",
-        "qian2617..com",
-        "qian_chen_jin@lynchow.com",
-        "jin",
-        "test",
-        "test",
-        "smtp.lynchow.com",
-        ["chenjin.qian@xquant.com", "qian_chen_jin@163.com"],
-        ["jin", "jin1"]
-    )
-    a.send_mail()
